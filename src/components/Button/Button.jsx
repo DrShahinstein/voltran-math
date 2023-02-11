e584932b36f62e12ffaf37db5897579e2c9068ee
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-12 00:05:47 +0300

    Provide functionality of adjusting button scale

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,22 @@
+import React from "react";
 import "./button.css";
 
 export default function Button(props) {
+  if (props.small)
+    return (
+      <button type={props.type} className="btn-blue btn-small">
+        {props.children}
+      </button>
+    );
+  if (props.large)
+    return (
+      <button type={props.type} className="btn-blue btn-large">
+        {props.children}
+      </button>
+    );
   return (
-    <button type={props.type} className="btn-blue">
-      Submit
+    <button type={props.type} className="btn-blue btn-medium">
+      {props.children}
     </button>
   );
 }
