70f78a43df8b81a57ad111fceb6c1be2d95fc446
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-12 02:51:36 +0300

    Keep django secret key private

--- server/server/settings.py
+++ server/server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)@y&j4gf9ei96#5863_yf0_6)6@56(5x$cxr*u%6_z#6#@1t(i'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
