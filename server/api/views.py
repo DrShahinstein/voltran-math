04d2c41d26ddfc9af7e91cfca460eb50501cd651
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-13 01:34:01 +0300

    Create proper views

--- server/api/views.py
+++ server/api/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from core.models import Result
 from .serializers import ResultSerializer
 
 
-class ResultViewSet(viewsets.ReadOnlyModelViewSet):
+class ResultsAPIView(generics.ListAPIView):
     queryset = Result.objects.all()
+    serializer_class = ResultSerializer
+
+
+class CreateResultAPIView(generics.CreateAPIView):
     serializer_class = ResultSerializer
