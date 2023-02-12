683178870e057611d7365e1b3ae591d48d3eea06
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-13 01:36:05 +0300

    Create proper models providing dynamism using many-to-many

--- server/core/models.py
+++ server/core/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-from picklefield.fields import PickledObjectField
+
+
+class Input(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.TextField()
+
+
+class Output(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.TextField()
 
 
 class Result(models.Model):
-    name = models.CharField("Result Name", max_length=100)
-    inputs = PickledObjectField()
-    outputs = PickledObjectField()
-
-    def __str__(self):
-        return self.name
+    result_name = models.CharField(max_length=100, default="RESULT_NAME")
+    inputs = models.ManyToManyField(Input)
+    outputs = models.ManyToManyField(Output)
