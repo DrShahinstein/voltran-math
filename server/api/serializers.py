79e50e08c8f83f544da1aaf4e66da649123d2fc9
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-13 01:33:40 +0300

    Develop serializers

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from core.models import Result
+from core.models import Result, Input, Output
 
 
-class ResultSerializer(serializers.HyperlinkedModelSerializer):
+class InputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Input
+        fields = ('name', 'value')
+
+
+class OutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Output
+        fields = ('name', 'value')
+
+
+class ResultSerializer(serializers.ModelSerializer):
+    inputs = InputSerializer(many=True, read_only=True)
+    outputs = OutputSerializer(many=True, read_only=True)
+
     class Meta:
         model = Result
-        fields = ["name", "inputs", "outputs"]
+        fields = ('result_name', 'inputs', 'outputs')
