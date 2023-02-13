7be8cbe2b263133a3a6a95f33ba38a24b605117d
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-13 23:25:27 +0300

    Add ResultDeleteView to the urlpatterns for http delete reqs

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import ResultsAPIView, CreateResultAPIView
+from api.views import ResultsAPIView, CreateResultAPIView, ResultDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/results/', ResultsAPIView.as_view(), name='results'),
+    path('api/results/', ResultsAPIView.as_view(), name='get_result'),
     path('api/results/create/', CreateResultAPIView.as_view(), name='create_result'),
+    path('api/results/<str:result_name>/',
+         ResultDeleteView.as_view(), name='result_delete'),
 ]
