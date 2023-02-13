a71ca7b42669a19702b25e997de1233f2ea214a3
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-13 23:23:54 +0300

    Handle http delete requests

--- server/api/views.py
+++ server/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from core.models import Result
 from .serializers import ResultSerializer
 
@@ -10,3 +11,17 @@ class ResultsAPIView(generics.ListAPIView):
 
 class CreateResultAPIView(generics.CreateAPIView):
     serializer_class = ResultSerializer
+
+
+class ResultDeleteView(generics.DestroyAPIView):
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
+
+    def delete(self, request, *args, **kwargs):
+        result_name = self.kwargs.get('result_name')
+        result = self.queryset.filter(result_name=result_name).first()
+        if result:
+            result.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
