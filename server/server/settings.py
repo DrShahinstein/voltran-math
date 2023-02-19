13130bbcd77ccc00e357d52869037c3fe774d604
DrShahinstein <1taylanozturk@protonmail.com>
2023-02-19 23:33:28 +0300

    Declare STATIC_ROOT

--- server/server/settings.py
+++ server/server/settings.py
@@ -111,8 +111,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATIC_ROOT = "static"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
