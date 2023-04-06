dd885569a8017ccdb03b55d534263ce2b9138012
Taylan Öztürk <81323808+DrShahinstein@users.noreply.github.com>
2023-04-06 16:35:43 +0300

    Allow floating numbers in inputs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,6 +14,7 @@ const Input = forwardRef((props, ref) => {
     <label className="block mb-2">
       <input
         type="number"
+        step="any"
         className="input"
         placeholder={props.children}
         ref={ref}
