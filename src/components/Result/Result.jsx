2adb65f93232a53d0f6a04be78d7c145aa8ab542
DrShahinstein <1taylanozturk@protonmail.com>
2023-11-15 20:17:44 +0300

    Remaster the <Result> component for a more expanded calculation view

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -7,10 +7,17 @@ import "./result.css";
 const API = process.env.REACT_APP_API_URL;
 
 export default function Result(props) {
-  const inputs = props.inputs;
-  const inputCount = inputs.length;
-  const outputs = props.outputs;
-  const outputCount = outputs.length;
+  const advInputs = props.advInputs;
+  const advOutputs = props.advOutputs;
+  const stdInputs = props.stdInputs;
+  const stdOutputs = props.stdOutputs;
+  const advInputsCount = advInputs.length;
+  const stdInputsCount = stdInputs.length;
+  const advOutputsCount = advOutputs.length;
+  const stdOutputsCount = advOutputs.length;
+  // The below are used to declare what fields does an input or output structure have, allows easy mapping in JSX
+  const inputFields = ["power", "hours", "lampsCount", "lampsUnitPrice"];
+  const outputFields = ["energyConsumed", "cost"];
 
   const resultNameRef = useRef("result_name");
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -38,8 +45,10 @@ export default function Result(props) {
     await axios
       .post(`${API}/results/create/`, {
         result_name: resultName,
-        inputs: inputs,
-        outputs: outputs,
+        advInputs,
+        advOutputs,
+        stdInputs,
+        stdOutputs,
       })
       .then(() => {
         setModalContent(content);
@@ -49,16 +58,17 @@ export default function Result(props) {
         if (err.response.status === 400) {
           content.text = (
             <span>
-              There's already a result with the name <code>{resultName}</code>.
-              Result names must be unique.
+              A result with the name <code>{resultName}</code> is already
+              available Names must be unique
             </span>
           );
           setModalContent(content);
         } else {
-          content.text = "Something went wrong.";
+          content.text = "Something went wrong with the server";
           setModalContent(content);
         }
       });
+
     setIsModalVisible(true);
     resultNameRef.current.value = ""; // Reset <input>
   };
@@ -69,7 +79,7 @@ export default function Result(props) {
         {props.header ? (
           props.header
         ) : (
-          <header className="result-header w-11/12 sm:w-2/3 lg:w-1/2 xl:w-1/3 !rounded-b-none">
+          <header className="result-header">
             <input
               type="text"
               className="result-header--input"
@@ -84,32 +94,74 @@ export default function Result(props) {
           </header>
         )}
         <div className="result-container--inner !rounded-t-none">
-          <div className="mb-6">
-            <h2 className="result-title">In [{inputCount}]</h2>
-            <div className="result-content">
-              {inputs.map((input) => {
-                return (
-                  <div className="result-content--inner" key={input.name}>
-                    <h3 className="result-content-name">{input.name}</h3>
-                    <div className="result-content-value">{input.value}</div>
-                  </div>
-                );
-              })}
+          <section>
+            <h1 className="result-section-title">Advanced Lights</h1>
+            <div className="mb-6">
+              <h2 className="result-title">In [{advInputsCount}]</h2>
+              <div className="result-content">
+                {advInputs.map((input, i) => {
+                  return (
+                    <div className="result-content--inner" key={i}>
+                      <h3 className="result-content-key">{input.name}</h3>
+                      <div className="result-content-value">
+                        {input[inputFields[i]]}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-          </div>
-          <div className="mb-6">
-            <h2 className="result-title">Out [{outputCount}]</h2>
-            <div className="result-content">
-              {outputs.map((input) => {
-                return (
-                  <div className="result-content--inner" key={input.name}>
-                    <h3 className="result-content-name">{input.name}</h3>
-                    <div className="result-content-value">{input.value}</div>
-                  </div>
-                );
-              })}
+            <div className="mb-6">
+              <h2 className="result-title">Out [{advOutputsCount}]</h2>
+              <div className="result-content">
+                {advOutputs.map((output, i) => {
+                  return (
+                    <div className="result-content--inner" key={i}>
+                      <h3 className="result-content-key">{output.name}</h3>
+                      <div className="result-content-value">
+                        {output[outputFields[i]]}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
-          </div>
+          </section>
+
+          <section>
+            <h1 className="result-section-title">Standart Lights</h1>
+            <div className="mb-6">
+              <h2 className="result-title">In [{stdInputsCount}]</h2>
+              <div className="result-content">
+                {stdInputs.map((input, i) => {
+                  return (
+                    <div className="result-content--inner" key={i}>
+                      <h3 className="result-content-key">{input.name}</h3>
+                      <div className="result-content-value">
+                        {input[inputFields[i]]}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="mb-6">
+              <h2 className="result-title">Out [{stdOutputsCount}]</h2>
+              <div className="result-content">
+                {stdOutputs.map((output, i) => {
+                  return (
+                    <div className="result-content--inner" key={i}>
+                      <h3 className="result-content-key">{output.name}</h3>
+                      <div className="result-content-value">
+                        {output[outputFields[i]]}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+
           <footer className="result-footer">
             <p>
               Check out the source code on{" "}
