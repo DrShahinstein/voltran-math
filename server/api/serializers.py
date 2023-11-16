f7fabceefe9069c25a4fa446551785e03f53abc5
DrShahinstein <1taylanozturk@protonmail.com>
2023-11-16 16:27:37 +0300

    Edit fields to be serialized as per re-defined fields

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,46 +1,15 @@
+from core.models import Data
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from core.models import Result, Input, Output
+from core.models import Result
 
 
-class InputSerializer(serializers.ModelSerializer):
+class DataSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Input
-        fields = ('name', 'value')
-
-
-class OutputSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Output
-        fields = ('name', 'value')
+        model = Data
+        fields = ["data_type", "lighting_type", "field_type", "value"]
 
 
 class ResultSerializer(serializers.ModelSerializer):
-    inputs = InputSerializer(many=True)
-    outputs = OutputSerializer(many=True)
-
     class Meta:
         model = Result
-        fields = ('result_name', 'inputs', 'outputs')
-
-    def create(self, validated_data):
-        result_name = validated_data["result_name"]
-        inputs = validated_data["inputs"]
-        outputs = validated_data["outputs"]
-
-        if Result.objects.filter(result_name=result_name).exists():
-            raise ValidationError(
-                f"Result with name {result_name} already exists.")
-
-        result = Result(result_name=result_name)
-        result.save()
-
-        for input_data in inputs:
-            input = Input.objects.create(**input_data)
-            result.inputs.add(input)
-
-        for output_data in outputs:
-            output = Output.objects.create(**output_data)
-            result.outputs.add(output)
-
-        return result
+        fields = ["title", "description"]
